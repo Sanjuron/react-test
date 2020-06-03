6f2f04056a050e74ec7adc63d7bf6df40fc186f5
sanjuron <julien_nn@live.fr>
2020-06-03 11:42:18 +0200

    netninja/react

---
+++ js/app.js
@@ -0,0 +1,16 @@
+class App extends React.Component {
+
+
+     render() {
+         return (
+            <div className="app-content"> 
+                <h1>Hey, Ninjas</h1>
+                <p>{ Math.random() * 10}</p>
+            </div>
+         )
+     }
+
+}
+
+
+ReactDOM.render(<App/>,document.getElementById('app'));
