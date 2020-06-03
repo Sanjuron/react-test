ecc94575966e903740514551a110e09866350990
sanjuron <julien_nn@live.fr>
2020-06-03 16:11:01 +0200

    leçon 8 + ajouts personnels

--- js/app.js
+++ js/app.js
@@ -1,11 +1,44 @@
 class App extends React.Component {
+    
+    state = {
+        name: "Sanjuron",
+        age: 30
 
+    }
+
+    handleClick = e => {
+        // console.log(e.target);
+        this.setState(
+            {
+                name: prompt("entre ton nom:"),
+                age: prompt("entre ton age:")
+            }
+        )
+        console.log(this.state);}
+    
+
+    handleMouseOver = e => {
+        console.log(e.target, e.pageX);
+    }
+
+    handleCopy = e => {
+        alert("Try being original, for once");
+    }
+
+    handlePaste = e => {
+        alert("qu'est ce que t'essaies de faire malheureux!")
+        e.preventDefault;
+    }
 
      render() {
          return (
             <div className="app-content"> 
                 <h1>Hey, Ninjas</h1>
-                <p>{ Math.random() * 10}</p>
+                <p>Je m'appelle {this.state.name} et j'ai {this.state.age} ans</p>
+                <button onClick={this.handleClick}>Clique-moi</button>
+                <button onMouseOver={this.handleMouseOver}>Hover me</button>
+                <p onCopy={this.handleCopy}>What we think we become</p>
+                <input type="text" onPaste={this.handlePaste}/>
             </div>
          )
      }
