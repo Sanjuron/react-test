cd5e81def8b64a1c2d54000d9be7ed8fa2f6e90a
sanjuron <julien_nn@live.fr>
2020-06-06 15:56:27 +0200

    erreur

---
+++ my-app/src/Ninjas.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Ninjas extend Component {
+    render(){
+        return(
+            <div className="ninja">
+                <div>Name: Sanjuron</div>
+                <div>Age: 30</div>
+                <div>Belt: Yellow</div>
+            </div>
+        )
+    }
+}
+
+export default Ninjas
