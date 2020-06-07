12af5b9f79a5c1452bae8b19d2ee43a7272ac93e
sanjuron <julien_nn@live.fr>
2020-06-07 18:41:11 +0200

    leçon 18: functions as props + chgt App en class

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { Component} from 'react';
 import Ninjas from "./Ninjas";
 import AddNinja from "./AddNinja";
 // import './App.css';
 
-function App() {
-  const ninjas = [
+class  App extends Component {
+  state = {
+    ninjas : [
     { name: "Sanjuron", age: 30, belt: "black", id: 1 },
     { name: "Dovakhin", age: 18, belt: "ultrablack", id: 2 },
     { name: "Yojimbo", age: 43, belt: "green", id: 3 }
-  ]
+    ]
+  }
 
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+
+  render(){
   return (
         <div className="App">
      <h1>My first React app</h1>
      <p>Bienvenue!</p>
-     <Ninjas ninjas = {ninjas}/>
-     <AddNinja />
+     <Ninjas ninjas = {this.state.ninjas}/>
+     <AddNinja  addNinja={this.addNinja}/>
 
     </div>
   );
 }
-
+}
 
 export default App;
