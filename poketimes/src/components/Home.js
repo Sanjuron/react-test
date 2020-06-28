3e822ac86be4b1a6f861134d3960ccc3cbba3325
sanjuron <julien_nn@live.fr>
2020-06-28 19:01:14 +0200

    28/29 High Order Components & Axios

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Home = () => {
-    return (
-        <div className="container">
-            <h4 className="center">Page D'accueil</h4>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima nemo hic excepturi porro nisi eaque. Vitae repudiandae dicta ea consectetur repellat quae, quam cupiditate dolor alias, quis rem, voluptas nam!</p>
-        </div>
-    )
+
+class Home extends Component {
+    state = {
+        posts: [ ]
+    }
+
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(res => {
+            console.log(res);
+            this.setState({
+                posts: res.data.slice(0, 10)
+            })
+        }) //action asynchrone
+    }
+
+    render(){
+        const { posts} = this.state;
+        const postList = posts.length ? (
+            posts.map(post => {
+                return (
+                    <div className="post card" key={post.id}>
+                        <div className="card-content">
+                            <span className="card-title">{post.title}</span>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+            <div className="center">No post Yet</div>
+        )
+        return (
+            <div className="container">
+                <h4 className="center">Page D'accueil</h4>
+                {postList}
+            </div>
+        )
+    }
+
 }
 
 export default Home;
--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -2,9 +2,9 @@ import React, { PureComponent } from 'react';
 import { Link, NavLink, withRouter} from 'react-router-dom'; // pour gérer les changement de pages sans rechargements
 
 const Navbar = (props) => {
-    setTimeout(() => {
-        props.history.push('/about')
-    }, 2000)
+    // setTimeout(() => {
+    //     props.history.push('/about')
+    // }, 2000)
     return (
         <nav className="nav-wrapper red darken-3">
             <div className="container">
